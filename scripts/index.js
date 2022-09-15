e679c8497c5a479ac50a3d3c2876c92469b317c0
Dmitriy Demchenko <iam@ddemchenko.ru>
2022-09-15 19:33:49 +0300

    variant1

--- scripts/index.js
+++ scripts/index.js
@@ -6,15 +6,35 @@
 // message.textContent = namePatient;
 
 const fieldsetsWomen = Array.from(document.querySelectorAll('.form__block_gender_women'));
-const radioWomen = document.querySelector('.select__image');
+const fieldsetsMen = Array.from(document.querySelectorAll('.form__block_gender_men'));
+const radioWomen = document.getElementById('button_women');
+const radioMen = document.getElementById('button_men');
+const radioBoth = document.getElementById('button_both');
+
+function showForm(fieldset) {
+  fieldset.forEach((element) => {
+    element.classList.add('form__block_show');
+  });
+};
+
+function hideForm(fieldsets) {
+  fieldsets.forEach((element) => {
+    element.classList.remove('form__block_show');
+  });
+};
 
 
 radioWomen.addEventListener('click', (evt) => {
-     fieldsetsWomen.forEach((element) => {
-      element.classList.add('form__block_show');
+  showForm(fieldsetsWomen);
+  hideForm(fieldsetsMen);
+});
 
-    });
+radioMen.addEventListener('click', (evt) => {
+  showForm(fieldsetsMen);
+  hideForm(fieldsetsWomen);
+});
 
- // }
-
+radioBoth.addEventListener('click', (evt) => {
+  showForm(fieldsetsMen);
+  showForm(fieldsetsWomen);
 });
